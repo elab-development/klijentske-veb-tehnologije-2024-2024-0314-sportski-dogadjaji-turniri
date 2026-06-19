e77b4f7e73ea2f4803b1837872b7d8375f54487c
petarnikolic05 <petarnikolic05@icloud.com>
2026-06-19 22:54:08 +0200

    TurnirMenazder

--- src/modeli/turniri.ts
+++ src/modeli/turniri.ts
@@ -14,3 +14,67 @@ export interface TurnirInterface{
     urlSlike: string;
     opis: string;
 }
+
+export class TurnirMenadzer {
+    private turnir: TurnirInterface;
+    
+    constructor(turnir: TurnirInterface) {
+        this.turnir = turnir;
+    }
+
+    public naziv(): string {
+        return this.turnir.naziv;
+    }
+
+    public sport(): string {
+        return this.turnir.sport;
+    }
+
+    public format(): string {
+        return this.turnir.format;
+    }
+
+    public lokacija(): string {
+        return this.turnir.lokacija;
+    }
+
+    public datumPocetka(): Date {
+        return this.turnir.datumPocetka;
+    }
+
+    public datumZavrsetka(): Date {
+        return this.turnir.datumZavrsetka;
+    }
+
+    public maxTimova(): number {
+        return this.turnir.maxTimova;
+    }
+
+    public prijavljenoTimova(): number {
+        return this.turnir.prijavljenoTimova;
+    }
+
+    public slobodnaMesta(): number {
+        return this.turnir.maxTimova - this.turnir.prijavljenoTimova;
+    }
+
+    public kotizacija(): number {
+        return this.turnir.kotizacija;
+    }
+
+    public nagradniFond(): number {
+        return this.turnir.nagradniFond;
+    }
+
+    public status(): string {
+        return this.turnir.status;
+    }
+    
+    public urlSlike(): string {
+        return this.turnir.urlSlike;
+    }
+    
+    public opis(): string {
+        return this.turnir.opis;
+    }
+}
